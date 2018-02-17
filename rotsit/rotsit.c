f6c953a9237eaf8095b3455fe873cbb2809acbd9
lelanthran <lelanthran@5ca7420d-43d6-4efc-b8d8-1b29aa438a47>
2018-02-17 07:49:39 +0000

    Started a basic project. This is intended to work as a library so that in the future, anyone needing to build a GUI app or wanting to make this work as a listening server can easily do so.
    The initial release will be a cli app.
    
    
    
    git-svn-id: svn://46.101.19.90/sources/rotsit@2864 5ca7420d-43d6-4efc-b8d8-1b29aa438a47

---
+++ rotsit/rotsit.c
@@ -0,0 +1,4 @@
+
+#include "rotsit/rotsit.h"
+
+
---
+++ rotsit/rotsit.h
@@ -0,0 +1,18 @@
+
+#ifndef H_ROTSIT
+#define H_ROTSIT
+
+typedef struct rotsit_t rotsit_t;
+typedef struct rotrec_t retrec_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
+
