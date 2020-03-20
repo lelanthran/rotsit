1760708e96138da43f3f046543d67a907992b2d4
lelanthran <lelanthran@gmail.com>
2020-03-20 07:12:37 +0200

    PROGRESS: Simple interface in header created. Looks sufficient for what I need. NEXT: Implement the interface and test using cdb_test.c.

--- src/cdb.h
+++ src/cdb.h
@@ -3,19 +3,31 @@
 #define H_CDB
 
 #include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-typedef struct cdb_t cdb_t;
-
-#define CDB_CREATE      (1 << 0)
-#define CDB_READ        (1 << 1)
-#define CDB_WRITE       (1 << 2)
+typedef bool (cdb_filter_func_t) (const void *lhs, const void *rhs);
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-   cdb_t *cdb_open (const char *fname, uint32_t flags);
-   void cdb_close (cdb_t *cdb);
+   char **cdb_records_load (const char *fname, int *errcode);
+   void cdb_records_save (char **records);
+   void cdb_records_free (char **records);
+
+   char **cdb_records_filter (char **records, cdb_filter_func_t *fptr);
+
+   bool cdb_field_add (char **record, const char *name, const char *value);
+   bool cdb_field_mod (char **record, const char *name, const char *value);
+   bool cdb_field_del (char **record, const char *name);
+
+   char *cdb_field_find (char *record, const char *name);
+   void cdb_field_free (char *field);
+
+   char **cdb_field_list (char *record);
+   void cdb_field_list_free (char **field_names);
+
 
 
 #ifdef __cplusplus
